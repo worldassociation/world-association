13e9a01926aeab5e71dd74510f5531b1d3c3cc55
blaisekonya <blaisekonya@gmail.com>
2024-06-03 22:06:55 +0200

    b

--- apps/web/src/app/constants.ts
+++ apps/web/src/app/constants.ts
@@ -1,5 +1,5 @@
 import { createThirdwebClient, getContract } from "thirdweb";
-import { baseSepolia } from "thirdweb/chains";
+import { base } from "thirdweb/chains";
 import { SmartWalletOptions } from "thirdweb/wallets";
 
 // Replace this with your client ID string
@@ -7,31 +7,31 @@ import { SmartWalletOptions } from "thirdweb/wallets";
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 
 if (!clientId) {
-	throw new Error("No client ID provided");
+  throw new Error("No client ID provided");
 }
 
 export const client = createThirdwebClient({
-	clientId: clientId,
+  clientId: clientId,
 });
 
-export const chain = baseSepolia;
+export const chain = base;
 export const tokenDropAddress = "0xd64A548A82c190083707CBEFD26958E5e6551D18";
 export const editionDropAddress = "0x638263e3eAa3917a53630e61B1fBa685308024fa";
 export const editionDropTokenId = 0n;
 
 export const editionDropContract = getContract({
-	address: editionDropAddress,
-	chain,
-	client,
+  address: editionDropAddress,
+  chain,
+  client,
 });
 
 export const tokenDropContract = getContract({
-	address: tokenDropAddress,
-	chain,
-	client,
+  address: tokenDropAddress,
+  chain,
+  client,
 });
 
 export const accountAbstraction: SmartWalletOptions = {
-	chain,
-	sponsorGas: true,
+  chain,
+  sponsorGas: true,
 };
