bc257e4a1a77bb21721c410763e7aba1949d98ff
blaisekonya <blaisekonya@gmail.com>
2024-06-13 18:57:45 +0200

    Add test files for contracts

--- smart-contracts/test/Constitution.t.sol
+++ smart-contracts/test/Constitution.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Constitution} from "../src/Constitution.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Constitution public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new Constitution();
         counter.setNumber(0);
     }
 
