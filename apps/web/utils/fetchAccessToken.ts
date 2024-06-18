9f41f005f4a974f09236f11f0aac7db7eb297d54
blaisekonya <blaisekonya@gmail.com>
2024-06-19 01:51:28 +0200

    Add variables

--- apps/web/utils/fetchAccessToken.ts
+++ apps/web/utils/fetchAccessToken.ts
@@ -1,14 +1,17 @@
 // This function fetches a new access token from the specified API endpoint.
 
 export async function fetchAccessToken(): Promise<string> {
+  const apiKey = process.env.ZKME_API_KEY;
+  const appId = process.env.ZKME_APP_ID;
+
   return fetch("https://nest-api.zk.me/api/token/get", {
     method: "POST",
     headers: {
       "Content-type": "application/json",
     },
     body: JSON.stringify({
-      apiKey: process.env.ZKME_API_KEY,
-      appId: process.env.ZKME_APP_ID,
+      apiKey: apiKey,
+      appId: appId,
       apiModePermission: 1,
       lv: 2,
     }),
