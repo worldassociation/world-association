eddd7e5113eb95cb69505369bcf72a518d329db2
blaisekonya <blaisekonya@gmail.com>
2024-06-18 14:42:26 +0200

    Turn payment token into custom supertoken

--- smart-contracts/src/Drachma.sol
+++ smart-contracts/src/Drachma.sol
@@ -1,75 +1,60 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity 0.8.26;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {SuperTokenBase} from "../lib/custom-supertokens/contracts/base/SuperTokenBase.sol";
 
-contract Drachma is
-    Initializable,
-    ERC20Upgradeable,
-    ERC20BurnableUpgradeable,
-    ERC20PausableUpgradeable,
-    AccessControlUpgradeable,
-    ERC20PermitUpgradeable,
-    UUPSUpgradeable
-{
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+/// @title Burnable and Mintable Pure Super Token
+/// @author jtriley.eth
+/// @notice This does not perform checks when burning
+contract BurnMintSuperToken is SuperTokenBase, AccessControl {
+    /// @notice Minter Role
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
+    /// @notice Burner Role
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
     constructor() {
-        _disableInitializers();
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /// @notice Initializer, used AFTER factory upgrade
+    /// @dev We MUST mint here, there is no other way to mint tokens
+    /// @param factory Super Token factory for initialization
+    /// @param name Name of Super Token
+    /// @param symbol Symbol of Super Token
+    /// @param initialSupply Initial token supply to pre-mint
+    /// @param receiver Receiver of pre-mint
+    /// @param userData Arbitrary user data for pre-mint
     function initialize(
-        address defaultAdmin,
-        address pauser,
-        address minter,
-        address upgrader
-    ) public initializer {
-        __ERC20_init("Drachma", "WDD");
-        __ERC20Burnable_init();
-        __ERC20Pausable_init();
-        __AccessControl_init();
-        __ERC20Permit_init("Drachma");
-        __UUPSUpgradeable_init();
-
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(PAUSER_ROLE, pauser);
-        _grantRole(MINTER_ROLE, minter);
-        _grantRole(UPGRADER_ROLE, upgrader);
+        address factory,
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address receiver,
+        bytes memory userData
+    ) external {
+        _initialize(factory, name, symbol);
+        _mint(receiver, initialSupply, userData);
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
+    /// @notice Mints tokens, only the owner may do this
+    /// @param receiver Receiver of minted tokens
+    /// @param amount Amount to mint
+    function mint(
+        address receiver,
+        uint256 amount,
+        bytes memory userData
+    ) external onlyRole(MINTER_ROLE) {
+        _mint(receiver, amount, userData);
     }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
-
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal override onlyRole(UPGRADER_ROLE) {}
-
-    // The following functions are overrides required by Solidity.
-
-    function _update(
-        address from,
-        address to,
-        uint256 value
-    ) internal override(ERC20Upgradeable, ERC20PausableUpgradeable) {
-        super._update(from, to, value);
+    /// @notice Burns from message sender
+    /// @param amount Amount to burn
+    function burn(
+        uint256 amount,
+        bytes memory userData
+    ) external onlyRole(BURNER_ROLE) {
+        _burn(msg.sender, amount, userData);
     }
 }
