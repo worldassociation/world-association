1075a4ba015c9845ec66ed7ab6dad5806ad4e225
blaisekonya <blaisekonya@gmail.com>
2024-06-24 10:13:21 +0200

    Update Drachma contract

--- smart-contracts/src/Drachma.sol
+++ smart-contracts/src/Drachma.sol
@@ -1,37 +1,61 @@
 // SPDX-License-Identifier: AGPLv3
-pragma solidity 0.8.19;
+pragma solidity ^0.8.0;
 
-import {ISuperToken, CustomSuperTokenBase} from "@superfluid/contracts/interfaces/superfluid/CustomSuperTokenBase.sol";
-import {IPureSuperTokenCustom} from "@superfluid/contracts/interfaces/tokens/IPureSuperToken.sol";
-import {UUPSProxy} from "@superfluid/contracts/upgradability/UUPSProxy.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-/**
- * @title Native SuperToken custom super token implementation
- * @author Superfluid
- * @dev This is a simple implementation where the supply is pre-minted.
- */
-contract PureSuperToken is
-    IPureSuperTokenCustom,
-    CustomSuperTokenBase,
-    UUPSProxy
-{
+import {SuperTokenBase} from "../lib/custom-supertokens/contracts/base/SuperTokenBase.sol";
+
+/// @title Burnable and Mintable Pure Super Token
+/// @author jtriley.eth
+/// @notice This does not perform checks when burning
+contract BurnMintSuperToken is SuperTokenBase, AccessControl {
+    /// @notice Minter Role
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @notice Burner Role
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    /// @notice Initializer, used AFTER factory upgrade
+    /// @dev We MUST mint here, there is no other way to mint tokens
+    /// @param factory Super Token factory for initialization
+    /// @param name Name of Super Token
+    /// @param symbol Symbol of Super Token
+    /// @param initialSupply Initial token supply to pre-mint
+    /// @param receiver Receiver of pre-mint
+    /// @param userData Arbitrary user data for pre-mint
     function initialize(
-        string calldata name,
-        string calldata symbol,
-        uint256 initialSupply
-    ) external override {
-        ISuperToken(address(this)).initialize(
-            // NOTE this is the verbose intention
-            IERC20(0x0000000000000000000000000000000000000000), // no underlying/wrapped token
-            18, // shouldn't matter if there's no wrapped token
-            name,
-            symbol
-        );
-        ISuperToken(address(this)).selfMint(
-            msg.sender,
-            initialSupply,
-            new bytes(0)
-        );
+        address factory,
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address receiver,
+        bytes memory userData
+    ) external {
+        _initialize(factory, name, symbol);
+        _mint(receiver, initialSupply, userData);
+    }
+
+    /// @notice Mints tokens, only the owner may do this
+    /// @param receiver Receiver of minted tokens
+    /// @param amount Amount to mint
+    function mint(
+        address receiver,
+        uint256 amount,
+        bytes memory userData
+    ) external onlyRole(MINTER_ROLE) {
+        _mint(receiver, amount, userData);
+    }
+
+    /// @notice Burns from message sender
+    /// @param amount Amount to burn
+    function burn(
+        uint256 amount,
+        bytes memory userData
+    ) external onlyRole(BURNER_ROLE) {
+        _burn(msg.sender, amount, userData);
     }
 }
