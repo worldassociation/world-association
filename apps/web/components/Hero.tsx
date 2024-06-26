81fbc3b076e0f0c0073948138fa6c29400fa427d
blaisekonya <blaisekonya@gmail.com>
2024-06-27 00:28:13 +0200

    Update wording

--- apps/web/components/Hero.tsx
+++ apps/web/components/Hero.tsx
@@ -76,10 +76,10 @@ export default function Hero() {
               <h1 className="text-[13vw] leading-snug pt-12 lg:pt-0">
                 Uniting humanity
               </h1>
-              <p className="text-[4.7vw] text-[#666] pt-2 pb-4 max-w-[608px]">
+              <p className="text-[4.7vw] text-[#666] pt-2 pb-4 max-w-[640px]">
                 The World Association is an open-source, verifiably democratic
-                global governance experiment that anyone can join completely
-                anonymously.
+                global governance experiment with an own digital currency that
+                anyone can join completely anonymously.
               </p>
             </>
           )}
